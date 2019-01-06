4dcc135352eb486022ada36f7a48950f50d38741
lni_T <developer.lni@gmail.com>
2019-01-07 00:45:53 +0900

    KEY PARTITION

--- db/migrate/20190106103104_create_t2_logs.rb
+++ db/migrate/20190106103104_create_t2_logs.rb
@@ -1,6 +1,6 @@
 class CreateT2Logs < ActiveRecord::Migration[5.2]
   def change
-    create_table :t2_logs do |t|
+    create_table :t2_logs, primary_key: [:company_guid, :uuid] do |t|
       t.string :uuid
       t.string :company_guid
       t.datetime :created_at, limit: 6
@@ -13,5 +13,6 @@ class CreateT2Logs < ActiveRecord::Migration[5.2]
     add_index :t2_logs, :name1
     add_index :t2_logs, :name2
     add_index :t2_logs, :name3
+    execute "ALTER TABLE t2_logs PARTITION BY KEY(company_guid) PARTITIONS 4;"
   end
 end
